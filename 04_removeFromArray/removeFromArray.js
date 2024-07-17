36d1e443f94d33dff4e2ecf14159e918c8afdfca
rafaelb056 <rafaelbenitez056@gmail.com>
2024-07-17 12:01:12 -0400

    add .

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(passedArray,...args) {
+  const newArray = []
+  passedArray.forEach(items => {
+    if(!args.includes(items)){
+      newArray.push(items)
+    }
+  })
+  return newArray;
 };
 
 // Do not edit below this line
